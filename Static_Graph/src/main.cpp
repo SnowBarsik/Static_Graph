e28173caf3e0fadd26d44719592a764f72f1cbac
sakeof.capy <non-linear@ukr.net>
2024-11-03 15:02:49 +0200

    finalize: diagram

--- Static_Graph/src/main.cpp
+++ Static_Graph/src/main.cpp
@@ -1,4 +1,14 @@
 #include <cstddef>
+#include <optional>
+
+using VertexType = int;
+
+template<typename T>
+struct Slice
+{
+	T* data;
+	std::size_t size;
+};
 
 // Constexpr graphs
 
@@ -26,6 +36,8 @@ public:
 	}
 
 	constexpr void add_edge(VertexType v1, VertexType v2) const {}
+
+	constexpr void adjacent(VertexType v) {}
 };
 
 // Template-based metaprogramming
@@ -42,8 +54,72 @@ struct HasEdgeUtility {};
 template<std::size_t Vertex, typename Graph>
 struct DegUtility {};
 
+template<std::size_t Vertex, typename Graph>
+struct AdjacentUtility {};
+
 template<typename Graph>
 struct EdgesCount {};
 
 template<typename Graph>
 struct VerticesCount {};
+
+// Algorithms
+
+template<
+	typename Graph, 
+	template<typename> typename F
+>
+struct BFS {};
+
+template<
+	typename Graph,
+	template<typename> typename F
+>
+struct DFS {};
+
+template<typename Graph>
+struct IsAcyclicGraph {};
+
+struct ConstexprGraphAlgorithm
+{
+	template<std::size_t V, typename F>
+	static auto dfs(ConstexprBasedGraph<V>& graph, VertexType v_start, F&& f) {
+		std::optional<Slice<VertexType>> opt = Slice<VertexType> { nullptr, 0 };
+		return opt;
+	}
+
+	template<std::size_t V, typename F>
+	static auto bfs(ConstexprBasedGraph<V>& graph, VertexType v_start, F&& f) {
+		return std::nullopt;
+	}
+
+	template<std::size_t V>
+	static bool is_acyclic(ConstexprBasedGraph<V>& graph) {
+		return false;
+	}
+};
+
+
+
+int main() {
+	ConstexprBasedGraph<10> graph;
+
+	//std::optional<int>
+
+	VertexType v_start = 0;
+	VertexType v_end = 10;
+
+	Slice<VertexType> found_path = *ConstexprGraphAlgorithm::dfs(
+		graph, 
+		v_start, 
+		[v_end](VertexType v, Slice<VertexType> current_path) -> std::optional<Slice<VertexType>> {
+			if (v == v_end) {
+				return current_path;
+			}
+
+			return std::nullopt;
+		}
+	);
+
+	return 0;
+}
